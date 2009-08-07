0b7612e40aecd69f4967edaea044bc4a863ab248
hguerreiro <hguerreiro@aadfa582-8358-11de-8ff2-33b0a5e28824>
2009-08-07 15:49:48 +0000

    Fixed the copyright header.

--- data/plugin/action/comment_approve.py
+++ data/plugin/action/comment_approve.py
@@ -3,14 +3,14 @@
 # Moin-comments - Blog like comments in MoinMoin
 # Copyright (C) 2009 José Lopes
 
-## This file is part of WebPyMail.
+## This file is part of Moin-comments.
 ##
-## WebPyMail is free software: you can redistribute it and/or modify
+## Moin-comments is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
 ##
-## WebPyMail is distributed in the hope that it will be useful,
+## Moin-comments is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
